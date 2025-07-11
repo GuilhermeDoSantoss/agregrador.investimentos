e943534a109ebb246b0be94b8f83dd517ea7e0a6
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-07-11 18:25:54 -0300

    projeto criado e vinculada ao banco de dados

---
+++ src/main/java/crud/java/agendadorinvestimentos/AgendadorinvestimentosApplication.java
@@ -0,0 +1,13 @@
+package crud.java.agendadorinvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorinvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorinvestimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crud/java/agendadorinvestimentos/AgendadorinvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package crud.java.agendadorinvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorinvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
