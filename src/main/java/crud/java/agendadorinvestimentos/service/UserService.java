420d56021dede522910ffb006cca157b33cfa247
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-07-12 12:38:41 -0300

    criado a funcionalidade de listar usuarios

--- src/main/java/crud/java/agendadorinvestimentos/service/UserService.java
+++ src/main/java/crud/java/agendadorinvestimentos/service/UserService.java
@@ -6,6 +6,7 @@ import crud.java.agendadorinvestimentos.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -38,5 +39,9 @@ public class UserService {
     public Optional<User> getUserById(String userId){
 
         return userRepository.findById(UUID.fromString(userId));
+    }
+
+    public List<User> listaUsers(){
+        return userRepository.findAll();
     }
 }
