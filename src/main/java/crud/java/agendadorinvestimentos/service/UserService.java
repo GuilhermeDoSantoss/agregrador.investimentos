fe3cde94900b438b7396fdfd77953f1518f8bb82
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-07-12 12:32:23 -0300

    criado funcionalidade de consultar usuário pelo id

--- src/main/java/crud/java/agendadorinvestimentos/service/UserService.java
+++ src/main/java/crud/java/agendadorinvestimentos/service/UserService.java
@@ -6,6 +6,7 @@ import crud.java.agendadorinvestimentos.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -32,5 +33,10 @@ public class UserService {
          var userSaved =  userRepository.save(entity);
 
          return userSaved.getUserId();
+    }
+
+    public Optional<User> getUserById(String userId){
+
+        return userRepository.findById(UUID.fromString(userId));
     }
 }
